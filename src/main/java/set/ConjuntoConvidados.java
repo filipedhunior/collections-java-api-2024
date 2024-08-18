9b5aefa3660aef36592102235bc10760e5c4389c
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-18 13:16:05 -0300

    [Update] ConjuntoConvidados
    Alterando os metodos e aplicando a logica
    
    Signed-off-by: Filipe Dhunior <pessoal.filiperocha@gmail.com>

--- src/main/java/set/ConjuntoConvidados.java
+++ src/main/java/set/ConjuntoConvidados.java
@@ -9,4 +9,31 @@ public class ConjuntoConvidados {
     public ConjuntoConvidados() {
         this.convidadoSet = new HashSet<>();
     }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public Set<ConjuntoConvidados> removerConvidadoPorCodigoConvite(int codigoConvite){
+        ConjuntoConvidados convidadosRemovidos = new ConjuntoConvidados();
+        for (ConjuntoConvidados c : Convidado){
+            if (c.getC)
+        }
+
+        return
+    }
+
+    public void contarConvidados(){
+        System.out.println(convidadoSet.size());
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados convidadoUm = new ConjuntoConvidados();
+        convidadoUm.adicionarConvidado("Filipe", 832004);
+        convidadoUm.adicionarConvidado("Filipe", 732005);
+        convidadoUm.adicionarConvidado("Samara", 21102005);
+
+        convidadoUm.contarConvidados();
+
+    }
 }
