7588306170b6e3f139cccc08e8566ca83e8dd0df
Filipe Dhunior <162770790+filipedhunior@users.noreply.github.com>
2024-08-20 14:58:12 -0300

    Set -  Exercicio 02 (#2)
    * [Update] apagando o pacote List
    
    * [New] ConjuntoPalavrasUnicas
    
    * [Update] Adição e correção de metodos

---
+++ src/main/java/set/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,48 @@
+package main.java.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    Set<String> palavras = new HashSet<String>();
+
+    public void adicionarPalavra(String palavra){
+        palavras.add(palavra);
+        System.out.println("Palavra [" + palavra + "] adicionada com sucesso!");
+    }
+//    Adiciona uma palavra ao conjunto.
+    public void removerPalavra(String palavra){
+        palavras.remove(palavra);
+    }
+//    Remove uma palavra do conjunto.
+    public void verificarPalavra(String palavra){
+        if (!palavras.contains(palavra)){
+            System.out.println("Não contem");
+        } else {
+            System.out.println("Contem a palavra " + palavra);
+        }
+    }
+//    Verifica se uma palavra está presente no conjunto.
+    public void exibirPalavrasUnicas(){
+        palavras.forEach(System.out::println);
+    }
+//    Exibe todas as palavras únicas do conjunto.
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjunto = new ConjuntoPalavrasUnicas();
+        conjunto.adicionarPalavra("ola");
+        conjunto.adicionarPalavra("hello");
+        conjunto.adicionarPalavra("nice!");
+
+        conjunto.verificarPalavra("ola");
+
+        conjunto.removerPalavra("hello");
+
+        conjunto.verificarPalavra("ola");
+
+        conjunto.exibirPalavrasUnicas();
+    }
+
+
+}
+
