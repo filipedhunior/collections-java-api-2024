3fadc5435a57b6b9d8b8c051fd984536490e30c1
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-22 14:55:39 -0300

    [Update] classes CadastroProdutos.java e Produto.java

--- src/main/java/set/Ordenação/CadastroProdutos.java
+++ src/main/java/set/Ordenação/CadastroProdutos.java
@@ -1,11 +1,24 @@
 package main.java.set.Ordenação;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class CadastroProdutos {
     Set<Produto> produto;
 
     public CadastroProdutos() {
         this.produto = new HashSet<>();
+    }
+
+
+    // METODOS
+    public void adicionarProduto(long cod, String nome, double preco, int quant) {
+        produto.add(new Produto(nome, cod, preco, quant));
+    }
+    public void exibirProdutoPorNome(){
+        // vai usar equals
+    }
+    public void exibirProdutosPorPreco(){
+        // criar um if onde cada numero precisa ser maior que o seu anterior. Caso seja maior, adicionar em uma lista
     }
 }
--- src/main/java/set/Ordenação/Produto.java
+++ src/main/java/set/Ordenação/Produto.java
@@ -2,7 +2,57 @@ package main.java.set.Ordenação;
 
 public class Produto {
     private String nome;
-    private int cod;
-    private double preço;
+    private long cod;
+    private double preco;
     private int quant;
+
+    public Produto(String nome, long cod, double preco, int quant) {
+        this.nome = nome;
+        this.cod = cod;
+        this.preco = preco;
+        this.quant = quant;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public void setCod(long cod) {
+        this.cod = cod;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    //    toString - SOBRESCEVER HASH DE MEMORIA
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", cod=" + cod +
+                ", preço=" + preco +
+                ", quant=" + quant +
+                '}';
+    }
 }
