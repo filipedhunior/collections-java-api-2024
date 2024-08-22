319498dbbb35e039452e9fe6a6cc9dc09a914543
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-22 14:43:34 -0300

    [New] Pacote Ordenação & classes CadastroProdutos.java e Produto.java

---
+++ src/main/java/set/Ordenação/CadastroProdutos.java
@@ -0,0 +1,11 @@
+package main.java.set.Ordenação;
+
+import java.util.HashSet;
+
+public class CadastroProdutos {
+    Set<Produto> produto;
+
+    public CadastroProdutos() {
+        this.produto = new HashSet<>();
+    }
+}
---
+++ src/main/java/set/Ordenação/Produto.java
@@ -0,0 +1,8 @@
+package main.java.set.Ordenação;
+
+public class Produto {
+    private String nome;
+    private int cod;
+    private double preço;
+    private int quant;
+}
