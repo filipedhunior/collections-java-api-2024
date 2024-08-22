d5c1c33047db1354671164340491cbf0551f42b2
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-22 16:16:38 -0300

    [New] classes GerenciadorAlunos & Aluno

---
+++ src/main/java/set/Ordenação/Aluno.java
@@ -0,0 +1,51 @@
+package main.java.set.Ordenação;
+
+public class Aluno implements Comparable<Aluno>{
+    @Override
+    public int compareTo(Aluno aluno) {
+        return nome.compareTo(aluno.nome);
+    }
+
+    private String nome;
+    private long matricula;
+    private double nota;
+
+    public Aluno(String nome, long matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(long matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", nota=" + nota +
+                '}';
+    }
+}
---
+++ src/main/java/set/Ordenação/GerenciadorAlunos.java
@@ -0,0 +1,33 @@
+package main.java.set.Ordenação;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class GerenciadorAlunos {
+    Set<Aluno> alunos;
+
+    public GerenciadorAlunos() {
+        alunos = new HashSet<>();
+    }
+
+    public void adicionarAluno(String nome, long matricula, double nota) {
+        alunos.add(new Aluno(nome, matricula, nota));
+    }
+
+    public void removerAluno(long matricula) {
+        if (alunos != null) {
+            for (Aluno aluno : alunos) {if (aluno.getMatricula() == matricula) {
+                    alunos.remove(aluno);
+                    break;
+                }
+            }
+        }
+        System.out.println("Aluno removido com sucesso!");
+    }
+
+    public Set<Aluno> exibirAlunosPorNome{
+        Set<Aluno> alunosPorNome = new TreeSet<>(alunos);
+        return alunosPorNome;
+    }
+}
