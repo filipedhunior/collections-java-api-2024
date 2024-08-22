45f9ca9d02611e75018b21bf3c8ba1ac20e0a457
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-22 16:47:25 -0300

    [Update] Alteração do metodo principal em relação ao metodo exibirAlunosPorNome na classe GerenciadorAlunos

--- src/main/java/set/Ordenação/GerenciadorAlunos.java
+++ src/main/java/set/Ordenação/GerenciadorAlunos.java
@@ -50,8 +50,8 @@ public class GerenciadorAlunos {
 
         gerenciador.adicionarAluno("Filipe" ,00L, 7.0);
         gerenciador.adicionarAluno("Junior" ,010L, 9.0);
-        gerenciador.adicionarAluno("Junior" ,05L, 6.5);
-        gerenciador.adicionarAluno("Junior" ,05L, 6.5);
+        gerenciador.adicionarAluno("Leandra" ,05L, 6.5);
+        gerenciador.adicionarAluno("Beatriz" ,05L, 6.5);
 
 //      gerenciador.exibirAlunos();
         System.out.println(gerenciador.exibirAlunosPorNome());
