4ba3ce06d9dbfde0c996d4a5d6b13c508b9ae323
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-23 16:53:12 -0300

    [Update] Corrigindo a sintaxe em Dicionario.java

--- src/main/java/map/operacoesBasicas/Dicionario.java
+++ src/main/java/map/operacoesBasicas/Dicionario.java
@@ -10,32 +10,7 @@ public class Dicionario {
         this.dicionario = new HashMap<>();
     }
 
-    public static void main(String[] args) {
-        Dicionario novoDicionario = new Dicionario();
-
-        novoDicionario.exbirPalavrras();
-
-        novoDicionario.adicionarPalavra("Correr", "Movimentar-se rapidamente sobre os pés.");
-        novoDicionario.adicionarPalavra("Computador", "Dispositivo eletrônico para processamento de dados.");
-        novoDicionario.adicionarPalavra("Livro", "Conjunto de folhas impressas ou manuscritas encadernadas.");
-        novoDicionario.adicionarPalavra("Cidade", "Área urbana com grande número de habitantes e infraestrutura.");
-        novoDicionario.adicionarPalavra("Música", "Arte de organizar sons e ritmos de forma agradável ao ouvido.");
-        novoDicionario.adicionarPalavra("Navegar", "Movimentar-se rapidamente sobre os pés.");
-        novoDicionario.adicionarPalavra("Estrutura", "Dispositivo eletrônico para processamento de dados.");
-
-
-        novoDicionario.exbirPalavrras();
-
-        novoDicionario.removerPalavra("Navegar");
-
-        novoDicionario.exbirPalavrras();
-
-        System.out.println(novoDicionario.pesquisarPorPalavra("Computador"));
-        System.out.println(novoDicionario.pesquisarPorPalavra("Cidade"));
-
-        novoDicionario.exbirPalavrras();
-    }
-
+    //METODOS
     public void adicionarPalavra(String palavra, String definição){
         if (dicionario.containsKey(palavra)){
             System.out.println("A palavra ultlizada ja existe no sistema!");
@@ -43,13 +18,12 @@ public class Dicionario {
         else {dicionario.put(palavra, definição);}
 //        System.out.println("Palavra " + palavra + " adicionada com sucesso!");
     }
-
     public void removerPalavra(String palavra) {
         if (!dicionario.isEmpty()) {
             dicionario.remove(palavra);
         }
     }
-    public void exbirPalavrras(){
+    public void exbirPalavras(){
         System.out.println(dicionario.toString());
     }
     public Map<String, String> pesquisarPorPalavra(String Palavra) {
@@ -61,4 +35,30 @@ public class Dicionario {
                 }
             } return dicionarioPorPalavra;
         }
+
+    public static void main(String[] args) {
+        Dicionario novoDicionario = new Dicionario();
+
+        novoDicionario.exbirPalavras();
+
+        novoDicionario.adicionarPalavra("Correr", "Movimentar-se rapidamente sobre os pés.");
+        novoDicionario.adicionarPalavra("Computador", "Dispositivo eletrônico para processamento de dados.");
+        novoDicionario.adicionarPalavra("Livro", "Conjunto de folhas impressas ou manuscritas encadernadas.");
+        novoDicionario.adicionarPalavra("Cidade", "Área urbana com grande número de habitantes e infraestrutura.");
+        novoDicionario.adicionarPalavra("Música", "Arte de organizar sons e ritmos de forma agradável ao ouvido.");
+        novoDicionario.adicionarPalavra("Navegar", "Movimentar-se rapidamente sobre os pés.");
+        novoDicionario.adicionarPalavra("Estrutura", "Dispositivo eletrônico para processamento de dados.");
+
+
+        novoDicionario.exbirPalavras();
+
+        novoDicionario.removerPalavra("Navegar");
+
+        novoDicionario.exbirPalavras();
+
+        System.out.println(novoDicionario.pesquisarPorPalavra("Computador"));
+        System.out.println(novoDicionario.pesquisarPorPalavra("Cidade"));
+
+        novoDicionario.exbirPalavras();
     }
+}
