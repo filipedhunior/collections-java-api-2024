31b1d95a093882a7759e5c03f3834a277993bb69
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-23 17:19:31 -0300

    [New] Classe Produto.java e adição de Metodos em EstoqueProdutos.java

--- src/main/java/map/ordenacao/EstoqueProdutos.java
+++ src/main/java/map/ordenacao/EstoqueProdutos.java
@@ -1,4 +1,48 @@
 package main.java.map.ordenacao;
 
+import main.java.map.pesquisa.Produto;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProdutos {
+    Map<Long, Produto> produtosEstoque;
+
+    public EstoqueProdutos() {
+        this.produtosEstoque = new HashMap<>();
+    }
+
+    public void adicionarProduto(Long cod, String nome, int quant, Double preco){
+        Produto produto = new Produto(cod, nome, quant, preco);
+        produtosEstoque.put(cod, produto);
+    }
+
+    public void exibirProdutos(){
+        System.out.println(produtosEstoque.toString());
+    }
+
+    public int calcularValorTotalEstoque(){
+        // Receber a lista produtosEstoque
+        // Calcular seus valores individuais por suas quantidades
+        // Calcular esse resultado final
+        return 0;
+    }
+
+    public void obterProdutoMaisCaro(){
+        // Altere a saida da classe para Map
+        // Use comparator ou comparable
+    }
+    public void obterProdutoMaisBarato(){
+        // Altere a saida da classe para Map
+        // Use comparator ou comparable
+    }
+
+    public void obterProdutoMaiorQuantValorEstoque(){
+        // Retorna o produto que está em maior quantidade no estoque, considerando o valor total de cada produto (quantidade * preço).
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/map/pesquisa/Produto.java
@@ -0,0 +1,57 @@
+package main.java.map.pesquisa;
+
+public class Produto{
+    private Long cod;
+    private String nome;
+    private int quant;
+    private double preco;
+
+    public Produto(Long cod, String nome, int quant, double preco) {
+        this.cod = cod;
+        this.nome = nome;
+        this.quant = quant;
+        this.preco = preco;
+    }
+
+    public Long getCod() {
+        return cod;
+    }
+
+    public void setCod(Long cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "cod=" + cod +
+                ", nome='" + nome + '\'' +
+                ", quant=" + quant +
+                ", preco=" + preco +
+                '}';
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
