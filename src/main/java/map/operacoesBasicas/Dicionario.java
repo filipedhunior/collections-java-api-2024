46035a9775838b7a1fe7759dfd97dfc54058e66d
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-23 16:22:29 -0300

    [Update] Criação dos metodos em Dicionario.java

--- src/main/java/map/operacoesBasicas/Dicionario.java
+++ src/main/java/map/operacoesBasicas/Dicionario.java
@@ -1,4 +1,62 @@
 package main.java.map.operacoesBasicas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Dicionario {
+    private Map<String, String> dicionario;
+
+    public Dicionario() {
+        this.dicionario = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definição){
+        if (dicionario.containsKey(palavra)){
+            System.out.println("A palavra ultlizada ja existe no sistema!");
+        }
+        else {dicionario.put(palavra, definição);}
+        System.out.println("Palavra" + palavra + " adicionada com sucesso!");
+    }
+
+    public void removerPalavra(String palavra) {
+        if (!dicionario.isEmpty()) {
+            dicionario.remove(palavra);
+        }
+    }
+    public void exbirPalavrras(){
+        System.out.println(dicionario.toString());
+    }
+    public Map<String, String> pesquisarPorPalavra(String Palavra) {
+        Map<String, String> dicionarioPorPalavra = new HashMap<>();
+
+        if (!dicionario.isEmpty()) {
+            for (String palavra : dicionario.keySet()) {
+                if (dicionario.containsKey(palavra)) {
+                    dicionarioPorPalavra.put(palavra, dicionario.get(palavra));
+                }
+            }
+        } return dicionarioPorPalavra;
+    }
+
+    public static void main(String[] args) {
+        Dicionario novoDicionario = new Dicionario();
+
+        novoDicionario.exbirPalavrras();
+
+        novoDicionario.adicionarPalavra("Correr", "Movimentar-se rapidamente sobre os pés.");
+        novoDicionario.adicionarPalavra("Computador", "Dispositivo eletrônico para processamento de dados.");
+        novoDicionario.adicionarPalavra("Livro", "Conjunto de folhas impressas ou manuscritas encadernadas.");
+        novoDicionario.adicionarPalavra("Cidade", "Área urbana com grande número de habitantes e infraestrutura.");
+        novoDicionario.adicionarPalavra("Música", "Arte de organizar sons e ritmos de forma agradável ao ouvido.");
+        novoDicionario.adicionarPalavra("Navegar", "Movimentar-se rapidamente sobre os pés.");
+        novoDicionario.adicionarPalavra("Estrutura", "Dispositivo eletrônico para processamento de dados.");
+
+
+//        novoDicionario.exbirPalavrras();
+//
+//        novoDicionario.removerPalavra("Correr");
+//
+//        novoDicionario.exbirPalavrras();
+
+    }
 }
