35710aeef813887a4152df89829bc621fbd18713
Filipe Dhunior <pessoal.filiperocha@gmail.com>
2024-08-23 15:36:04 -0300

    [Update] Criação dos metodos do AgendaContatos.java

--- src/main/java/map/operacoesBasicas/AgendaContatos.java
+++ src/main/java/map/operacoesBasicas/AgendaContatos.java
@@ -1,4 +1,41 @@
 package main.java.map.operacoesBasicas;
 
+import main.java.set.Pesquisa.Contato;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AgendaContatos {
+    private Map<String, Integer> contatos;
+    // String = nome & Integer = numeroTelefone
+    // Não posso usar int no gererics
+
+
+    public AgendaContatos() {
+        this.contatos = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        this.contatos.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        // TODO: removendo contato usando somente sua chave
+        this.contatos.remove(nome);
+        System.out.println("Contato removido com sucesso!");
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatos.toString());
+        // toString vai fazer o mesmo processo de sobrescrita?
+    }
+
+    public void pesquisarPorNome(String nome){
+        for(Map.Entry<String, Integer> contato : this.contatos.entrySet()){
+            if(contato.getKey().equals(nome)){
+                System.out.println("O contato é :" + contato.getKey());
+                System.out.println(contato.getValue());;
+            }
+        }
+    }
 }
