8f391aca768b575863a7df6d0175edb118aaa453
filipedhunior <pessoal.filiperocha@gmail.com>
2024-08-29 16:03:51 -0300

    [Update] obterProximoEvento
    Tentando entender a complexidade de usar LocalDate com regra de negocio
    
    Signed-off-by: filipedhunior <pessoal.filiperocha@gmail.com>

--- src/main/java/map/ordenacao/AgendaEventos.java
+++ src/main/java/map/ordenacao/AgendaEventos.java
@@ -1,11 +1,10 @@
 package main.java.map.ordenacao;
 
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AgendaEventos {
-    Map<LocalDate, Evento> eventoMap;
+    private Map<LocalDate, Evento> eventoMap;
 
     public AgendaEventos() {
         this.eventoMap = new HashMap<>();
@@ -16,10 +15,36 @@ public class AgendaEventos {
     }
 
     public void exibirAgenda(){
-        System.out.println(eventoMap);
+       // Exibe a agenda de forma crescente
+        // TreeMap é a implementação ideal para ordernar
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventosTreeMap);
     }
 
     public void obterProximoEvento(LocalDate data){
+//        Set<LocalDate> dataSet = eventoMap.keySet();
+//        Collection<Evento> values = eventoMap.values();
 
+        LocalDate dataAtual = LocalDate.now(); // retorna a data atual
+        LocalDate proximaData = null; // recebe o value do proximo evento em relação a data atual
+        Evento proximoEvento = null; // recebe o evento (nome) em relação a data atual
+
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap); //Instancia de TreeMap que recene Key e Value num unico objeto e instancia um novoTreeMap usando a lista passada como referencia
+        for (Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){// entrySet = retorna um set
+            if (entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O proximo evento é :" + proximoEvento + " na data " + proximaData);
+                break;
+            } // se o o objeto instancia em TreeMap tiver uma chave iguai ao dia atual ou antes dele
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.adicionarEvento(LocalDate.of(1822,9,07), "Independencia do Brasil", "Pedro Alvares Cabral");
+        agendaEventos.exibirAgenda();
     }
 }
--- src/main/java/map/ordenacao/Evento.java
+++ src/main/java/map/ordenacao/Evento.java
@@ -1,8 +1,16 @@
 package main.java.map.ordenacao;
 
 public class Evento {
-    String nome;
-    String atracao;
+    private String nome;
+    private String atracao;
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "nome='" + nome + '\'' +
+                ", atracao='" + atracao + '\'' +
+                '}';
+    }
 
     public Evento(String nome, String atracao) {
         this.nome = nome;
