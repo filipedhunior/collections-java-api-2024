822d85a08d8ae398b62f515741d3b2b82dd70501
filipedhunior <pessoal.filiperocha@gmail.com>
2024-08-29 15:18:01 -0300

    [Update] Melhorando ContagemPalavras
    Signed-off-by: filipedhunior <pessoal.filiperocha@gmail.com>

--- src/main/java/map/pesquisa/ContagemPalavras.java
+++ src/main/java/map/pesquisa/ContagemPalavras.java
@@ -1,5 +1,7 @@
 package main.java.map.pesquisa;
 
+import com.sun.jdi.Value;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,7 +25,43 @@ public class ContagemPalavras {
     public void exibirContagemPalavras(){
         System.out.println(contagemPalavras.toString());;
     }
-    public void encontrarPalavraFrequente(){
-        // iterar sobre os itens e comparar o numero de vezes que cada palavra foi usada e salvar numa variavel
+    public void encontrarPalavraFrequente() {
+        /*
+         * TODO: preciso encontrar a palavra dentro do map que corresponda ao maior Integer de dentro do mapa.
+         *   Dividindoa a tarefa fica:
+         *   Iterar sobre todas as chaves String
+         *   Comparar seus valores na iteração, vendo se ele é maior que o valor anterior
+         *   Manter sempre que o valor for o mais alto
+         *   Ao final, retornar a Palavra e o Valor que tem o maior valor
+         */
+        if (!contagemPalavras.isEmpty()) {
+            Integer valor = 0;
+            String palavra = "";
+            for (Map.Entry<String, Integer> palavraValorFrequente : contagemPalavras.entrySet()) {
+                if (palavraValorFrequente.getValue() > valor) {
+                    valor = palavraValorFrequente.getValue();
+                    palavra = palavraValorFrequente.getKey();
+                }
+            } System.out.println(palavra + " " + valor);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras palavra = new ContagemPalavras();
+        palavra.exibirContagemPalavras();
+
+        palavra.adicionarPalava("lorem", 4);
+        palavra.adicionarPalava("ipsum", 8); // tem qye ser retornado como o maior valor
+        palavra.adicionarPalava("dolor", 3);
+        palavra.adicionarPalava("sit", 2);
+
+        palavra.exibirContagemPalavras();
+
+        palavra.removerPalavra("ipsum");
+
+        palavra.exibirContagemPalavras();
+
+        palavra.encontrarPalavraFrequente();
     }
 }
